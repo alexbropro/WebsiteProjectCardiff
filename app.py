854ff5f3031ee77f062b97250f35ba6bc6449d0d
Alexander <sanyanga@cardiff.ac.uk>
2026-05-07 11:57:51 +0100

    final

--- app.py
+++ app.py
@@ -17,16 +17,17 @@ class Item(db.Model):
     image = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Float, nullable=False)
     author = db.Column(db.String(100), nullable=False)
+    eco_impact = db.Column(db.Float, nullable=False, default=0.0)
 
     def __repr__(self):
         return f'<Item {self.name}>'
 
     def seed(): 
         items = [
-            Item(name='Dragon Ball: Volume 1', description='Manga volume by Akira Toriyama', image='DB_1.png', price=7.99, author='Akira Toriyama'),
-            Item(name='Dragon Ball: Volume 3', description='Manga volume by Akira Toriyama', image='DB_3.jpg', price=7.99, author='Akira Toriyama'),
-            Item(name='Jujutsu Kaisen: Volume 1', description='Manga volume by Gege Akutami', image='JJK_!.PNG', price=8.99, author='Gege Akutami'),
-            Item(name='Hunter X Hunter: Volume 1', description='Manga volume by Yoshihiro Togashi', image='HXH_1.PNG', price=8.99, author='Yoshihiro Togashi'),
+            Item(name='Dragon Ball: Volume 1', description="Before there was \"Dragon Ball Z\", there was Akira Toriyama's action epic \"Dragon Ball\", starring the younger version of Son Goku and all the other \"Dragon Ball Z\" heroes! Meet a naive young monkey-tailed boy named Goku, whose quiet life changes when he meets Bulma, a girl who is on a quest to collect seven \"Dragon Balls.\" If she gathers them all, an incredibly powerful dragon will appear and grant her one wish. But the precious orbs are scattered all over the world, and Bulma needs Goku's help (and his super-strength)! With a magic staff for a weapon and a flying cloud for a ride, Goku sets out on the adventure of a lifetime…", image='DB_1.png', price=7.99, author='Akira Toriyama', eco_impact=2.5),
+            Item(name='Dragon Ball: Volume 3', description='With the Dragon Balls gone and Bulma\'s summer vacation over, Goku goes to the remote house of the Turtle Hermit, Kame-Sen\'nin, to be trained in the martial arts. There, the girl-ogling old master promises to teach Goku everything he knows…and prepare him for the Tenka\'ichi Budôkai, the great tournament to determine the Strongest Fighter in the World! But Goku\'s fellow student, the Shaolin monk Kuririn, isn\'t above cheating to be the best. Can the two of them get along as they undergo the strangest martial arts training ever?', image='DB_3.jpg', price=7.99, author='Akira Toriyama', eco_impact=2.0),
+            Item(name='Jujutsu Kaisen: Volume 1', description='Although Yuji Itadori looks like your average teenager, his immense physical strength is something to behold! Every sports club wants him to join, but Itadori would rather hang out with the school outcasts in the Occult Research Club. One day, the club manages to get their hands on a sealed cursed object. Little do they know the terror they will unleash when they break the seal...', image='JJK_1.PNG', price=8.99, author='Gege Akutami', eco_impact=1.8),
+            Item(name='Hunter X Hunter: Volume 1', description='Gon Freecss has almost reached the age of twelve, the earliest age at which one is allowed to register for the Hunter Exam. Despite the objections of his aunt Mito (also his adoptive mother), Gon sets off to follow in the footsteps of his father, the legendary Hunter Ging Freecss.', image='HXH_1.PNG', price=8.99, author='Yoshihiro Togashi', eco_impact=2.2),
         ]
         db.session.bulk_save_objects(items)
         db.session.commit()
@@ -108,7 +109,8 @@ def item_api(id):
         'description': item.description,
         'price': item.price,
         'author': item.author,
-        'image': item.image
+        'image': item.image,
+        'eco_impact': item.eco_impact
     })
 
 
@@ -127,7 +129,8 @@ def search_api():
         'name': item.name,
         'author': item.author,
         'image': item.image,
-        'price': item.price
+        'price': item.price,
+        'eco_impact': item.eco_impact
     } for item in results])
 
 
